fd651f63550e77bc9ee060c2336295faace0af98
ApplePills <x2pzjcydhc@privaterelay.appleid.com>
2026-08-30 07:15:10 +0800

    Regenerate Bilibili distribution bundle

--- src/script/bilibili/json/router.ts
+++ src/script/bilibili/json/router.ts
@@ -8,6 +8,7 @@ import {
     handleLiveFeedInfo,
     handleLiveRoomInfo,
     handleLiveUserInfo,
+    handleSearchSquare,
     handleSplash,
 } from './handler';
 import { initI18n, interceptor, initArgument } from './middleware';
@@ -16,7 +17,8 @@ const router = new Router({
     matchPath: matchPathSuffix,
 });
 
-router.get('/show/tab/v2', initI18n, handleLayout);
+router.get('/show/tab/v2', initArgument, initI18n, handleLayout);
+router.get('/search/square', initArgument, handleSearchSquare);
 router.get(['/splash/list', '/splash/show', '/splash/event/list2'], interceptor, handleSplash);
 router.get('/feed/index', handleFeedIndex);
 router.get('/feed/index/story', handleFeedIndexStory);
